b61e0b5d51c62515dbbf3e6a9f0d197fed0d8cd4
zuoshifan <zuoshifan@163.com>
2014-12-24 21:32:37 +0800

    Modify bb_matrix.py

--- bb_matrix.py
+++ bb_matrix.py
@@ -5,37 +5,50 @@
 :Authors: Shifan Zuo
 :Date: 2014-06-21
 :email: sfzuo@bao.ac.cn
-:usage:
-    python bb_matrix.py [-h] [-r [ROOT_DIR]] [-f [FILENAME]] [-m [MI]] [-o [OUT_DIR]] [--figfmt FIGFMT] [-l FIGLENGTH] [-w FIGWIDTH]
 """
 
 import argparse
 
 
+def natpattern(n):
+    """Pattern that prints out a number upto `n` (natural number - no sign)."""
+    return ("%0" + repr(int(np.ceil(np.log10(n + 1)))) + "d")
+
+
 def visualize_bb(args):
     import os
-    from os.path import join
+    # from os.path import join
+    import re
     import numpy as np
     import h5py
     import matplotlib
     matplotlib.use('Agg')
     from matplotlib import pyplot as plt
 
-    root_dir = args.root_dir if args.root_dir.endswith('/') else args.root_dir + '/'
-    mi_len = len(str(args.mi))
-    abs_filename = None
-    for dirpath, subdirnames, filenames in os.walk(args.root_dir):
-        for filename in filenames:
-            if filename == args.filename and int(dirpath.split('/')[-1]) == args.mi:
-                abs_filename = join(dirpath, filename)
-                mi_subdir = dirpath.split('/')[-1]
+    from drift.util import mpiutil
 
-    if abs_filename is None:
-        raise Exception('No file %s for mi = %d' % (args.filename, args.mi))
+    # root_dir = args.root_dir if args.root_dir.endswith('/') else args.root_dir + '/'
+    # mi_len = len(str(args.mi))
+    # abs_filename = None
+    # for dirpath, subdirnames, filenames in os.walk(args.root_dir):
+    #     for filename in filenames:
+    #         if filename == args.filename and int(dirpath.split('/')[-1]) == args.mi:
+    #             abs_filename = join(dirpath, filename)
+    #             mi_subdir = dirpath.split('/')[-1]
 
-    with h5py.File(abs_filename, 'r') as f:
+    # if abs_filename is None:
+    #     raise Exception('No file %s for mi = %d' % (args.filename, args.mi))
+
+    mi = re.search('[0-9]+', args.filename).group(0)
+    mi_subdir = mi
+
+    with h5py.File(args.filename, 'r') as f:
         beam_m = f['beam_m'][...]
-        freqs = f.attrs['frequencies']
+
+    pardir = os.path.abspath(os.path.join(os.path.dirname(args.filename), os.pardir)) # get parent dir
+    with h5py.File(pardir + '/telescope_data.hdf5', 'r') as f:
+        freqs = f['frequencies'][...]
+
     shape = beam_m.shape
     nfreq = shape[0]
     ntel = shape[1]*shape[2]
@@ -49,35 +62,38 @@ def visualize_bb(args):
         os.makedirs(outdir + mi_subdir)
 
     # plot
-    for fi in range(nfreq):
+    for fi in mpiutil.mpirange(nfreq):
         print '%d of %d...' % (fi, nfreq)
         plt.figure(figsize=(args.figlength, args.figwidth))
         plt.subplot(121)
-        plt.pcolor(bb_m[fi].real)
+        plt.pcolor(bb_m[fi].real, vmin=args.min, vmax=args.max)
         plt.xlim(0, nsky)
         plt.ylim(0, nsky)
         plt.xlabel(r"$(X'l')$")
         plt.ylabel('$(Xl)$')
-        plt.title(r"$\Re\left[\left(\mathbf{B}^{\dagger}\mathbf{B}\right)_{(Xl) (X'l')}\right]$ for $m = %d$ and $\nu = %.1f$ MHz" % (args.mi, freqs[fi]))
+        plt.title(r"$\Re\left[\left(\mathbf{B}^{\dagger}\mathbf{B}\right)_{(Xl) (X'l')}\right]$ for $m = %d$ and $\nu = %.1f$ MHz" % (int(mi), freqs[fi]))
         plt.colorbar()
 
         plt.subplot(122)
-        plt.pcolor(bb_m[fi].imag)
+        plt.pcolor(bb_m[fi].imag, vmin=args.min, vmax=args.max)
         plt.xlim(0, nsky)
         plt.ylim(0, nsky)
         plt.xlabel(r"$(X'l')$")
         plt.ylabel('$(Xl)$')
-        plt.title(r"$\Im\left[\left(\mathbf{B}^{\dagger}\mathbf{B}\right)_{(Xl) (X'l')}\right]$ for $m = %d$ and $\nu = %.1f$ MHz" % (args.mi, freqs[fi]))
+        plt.title(r"$\Im\left[\left(\mathbf{B}^{\dagger}\mathbf{B}\right)_{(Xl) (X'l')}\right]$ for $m = %s$ and $\nu = %.1f$ MHz" % (mi, freqs[fi]))
         plt.colorbar()
-        plt.savefig(outdir + mi_subdir + '/bb_%d_%d.%s' % (args.mi, fi), args.figfmt)
+        plt.savefig(outdir + mi_subdir + '/bb_%s_%d.%s' % (mi, fi, args.figfmt))
+        plt.close()
 
 
 parser = argparse.ArgumentParser(description='Visualize matrix B^{dagger}B.')
-parser.add_argument('-r', '--root_dir', type=str, nargs='?', default='./', help='Input beam transfer matrix data files root directory.')
-parser.add_argument('-f', '--filename', nargs='?', default='beam.hdf5', help='Input beam transfer matrix data file name.')
-parser.add_argument('-m', '--mi', type=int, nargs='?', default=0, help='m index.')
+# parser.add_argument('-r', '--root_dir', type=str, nargs='?', default='./', help='Input beam transfer matrix data files root directory.')
+parser.add_argument('filename', nargs='?', default='beam.hdf5', help='Input beam transfer matrix data file name.')
+# parser.add_argument('-m', '--mi', type=int, nargs='?', default=0, help='m index.')
 parser.add_argument('-o', '--out_dir', nargs='?', default='bb/', help='Directory of output figures.')
 parser.add_argument('--figfmt', default='png', help='Output image format.')
+parser.add_argument('--min', type=float, help='The min value of the visualize range in the output image.')
+parser.add_argument('--max', type=float, help='The max value of the visualize range in the output image.')
 parser.add_argument('-l', '--figlength', type=float, default=13, help='Output figure length.')
 parser.add_argument('-w', '--figwidth', type=float, default=5, help='Output figure width.')
 parser.set_defaults(func=visualize_bb)

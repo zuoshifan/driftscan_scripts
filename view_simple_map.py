ef1c054f2a50ba50bd51db9a8a316726f60641c3
zuoshifan <zuoshifan@163.com>
2015-01-15 15:59:06 +0800

    Add gnomview to view_simple_map

--- view_simple_map.py
+++ view_simple_map.py
@@ -54,11 +54,26 @@ def visualize_map(args):
     if args.outfile:
         out_file = args.outfile
     else:
-        out_file = mapname.replace('.hdf5', '_'+data+'.'+args.figfmt)
+        if args.view == 'm':
+            out_file = mapname.replace('.hdf5', '_'+data+'_'+args.view+'.'+args.figfmt)
+        elif args.view == 'g':
+            if args.log:
+                out_file = mapname.replace('.hdf5', '_'+data+'_'+args.view+'_log'+'.'+args.figfmt)
+            else:
+                out_file = mapname.replace('.hdf5', '_'+data+'_'+args.view+'_lin'+'.'+args.figfmt)
 
     # Plot and save image
-    fig = plt.figure(1, figsize=(args.figlength,args.figwidth))
-    hpvisual.mollview(hpmap, fig=1, title='', min=args.min, max=args.max)
+    if args.view == 'm':
+        fig = plt.figure(1, figsize=(args.figlength,args.figwidth))
+        hpvisual.mollview(hpmap, fig=1, title='', min=args.min, max=args.max)
+    elif args.view == 'g':
+        fig = plt.figure(1, figsize=(8, 6))
+        if args.log:
+            healpy.gnomview(np.log10(hpmap), fig=1, title='', rot=(90.0, 45.0, 0.0), xsize=20000, reso=2.0, degree=False, min=args.min, max=args.max, notext=True)
+        else:
+            healpy.gnomview(hpmap, fig=1, title='', rot=(90.0, 45.0, 0.0), xsize=20000, reso=2.0, degree=False, min=args.min, max=args.max, notext=True)
+
+        # healpy.gnomview(hpmap, fig=1, title='', xsize=10000)
     # healpy.cartview(hpmap[ifreq][args.pol], fig=1, title='', min=args.min, max=args.max)
     # fig = plt.figure()
     # ax = fig.add_axes()
@@ -76,6 +91,8 @@ parser = argparse.ArgumentParser(description='Visualize a sky map in hdf5 files.
 parser.add_argument('mapfiles', type=str, nargs='+', help='Input hdf5 sky map files to visualize. If more than one, they will be first combined, i.e. added together.')
 parser.add_argument('-o', '--outfile', type=str, nargs='?', help='Name of the image file to save into. If not present, the output image file name will be auto created from the first input map file name and input args.')
 parser.add_argument('-d', '--data', type=str, choices=['a', 'r', 'i'], default='a', help='Which data to visualize, `a` for abs, `r` for real and `i` for image.')
+parser.add_argument('--log', action='store_true', help='Log plot if True')
+parser.add_argument('-v', '--view', type=str, choices=['m', 'g'], default='m', help='Which view, `m` for mollview, `g` for gnomview.')
 parser.add_argument('-f', '--figfmt', default='png', help='Output image format.')
 # parser.add_argument('-i', '--ifreq', type=int, default=0, help='Frequency channel to visualize (start from 0). Negative integer N means the last Nth channel.')
 # parser.add_argument('-p', '--pol', type=int, default=0, choices=range(4), help='Polarization component to visualize, 0 for I/T, 1 for Q, 2 for U, 3 for V.')

bb2b9767a19433ec6e553004da33635c919a4df2
zuoshifan <zuoshifan@163.com>
2015-05-13 15:27:19 +0800

    A new case for vsyn_beam.py

--- vsyn_beam.py
+++ vsyn_beam.py
@@ -21,7 +21,7 @@ from matplotlib.ticker import MultipleLocator
 
 # Read in arguments
 parser = argparse.ArgumentParser(description="Calculate v baselines distribution.")
-parser.add_argument('-c', '--case', type=int, choices=range(1, 5), default=1, help='Which array configuration.')
+parser.add_argument('-c', '--case', type=int, choices=range(1, 6), default=1, help='Which array configuration.')
 parser.add_argument('-a', '--auto_corr', action='store_false', help='Whether use auto correlation.')
 parser.add_argument('-f', '--figfmt', default='png', help='Output image format.')
 parser.add_argument('-l', '--figlength', type=float, default=8, help='Output figure length.')
@@ -32,7 +32,7 @@ args = parser.parse_args()
 
 
 
-conf = ['3x32_0.5', '3x32_1.0', '3x32_2.5', '31_32_33_1.0']
+conf = ['3x32_0.5', '3x32_1.0', '3x32_2.5', '31_32_33_1.0', '31_32_33_2.5']
 Nconf = len(conf)
 cyl1_fp = [] # feed v position for feed 1, unit: lambda
 cyl2_fp = []
@@ -67,9 +67,23 @@ elif args.case == 4:
     Nfeeds1 = 31
     Nfeeds2 = 32
     Nfeeds3 = 33
-    v1 = (Nfeeds2 - 1) * 1.0 / (Nfeeds1 - 1) # m
-    v2 = 1.0 # m
-    v3 = (Nfeeds2 - 1) * 1.0 / (Nfeeds3 - 1) # m
+    v2 = 1.0 # in unit of one wavelength
+    v1 = (Nfeeds2 - 1) * v2 / (Nfeeds1 - 1)
+    v3 = (Nfeeds2 - 1) * v2 / (Nfeeds3 - 1)
+    for fd1 in range(Nfeeds1):
+        cyl1_fp.append(fd1 * v1)
+    for fd2 in range(Nfeeds2):
+        cyl2_fp.append(fd2 * v2)
+    for fd3 in range(Nfeeds3):
+        cyl3_fp.append(fd3 * v3)
+# Case 5: 31+32+33 v = 2.5
+elif args.case == 5:
+    Nfeeds1 = 31
+    Nfeeds2 = 32
+    Nfeeds3 = 33
+    v2 = 2.5 # in unit of one wavelength
+    v1 = (Nfeeds2 - 1) * v2 / (Nfeeds1 - 1)
+    v3 = (Nfeeds2 - 1) * v2 / (Nfeeds3 - 1)
     for fd1 in range(Nfeeds1):
         cyl1_fp.append(fd1 * v1)
     for fd2 in range(Nfeeds2):
@@ -104,6 +118,7 @@ plt.plot(theta, vsynbeam(theta))
 ax = plt.gca()
 ax.xaxis.set_major_locator(MultipleLocator(30))
 ax.xaxis.set_minor_locator(MultipleLocator(10))
+ax.yaxis.set_minor_locator(MultipleLocator(0.1))
 plt.xlabel(r'$\theta$ / degree')
 plt.xlim(-90.0, 90.0)
 plt.ylim(0.0, 1.0)

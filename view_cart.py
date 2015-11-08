472c07bd752fe660558e2107b65294646f62d7b8
zuoshifan <zuoshifan@163.com>
2015-11-08 18:07:34 +0800

    view_cart work for multi_freq and pol

--- view_cart.py
+++ view_cart.py
@@ -30,10 +30,15 @@ def visualize_cart(args):
         lon_min = f.attrs['lon_min']
         lon_max = f.attrs['lon_max']
 
-    out_file = args.outfile or args.inmap.replace('.hdf5', '.' + args.figfmt)
+    if cart_map.ndim > 2:
+        cart_map = cart_map[args.ifreq, args.pol]
+        out_file = args.outfile or ((args.inmap.split('/')[-1]).split('.')[0] + '_' + str(args.ifreq) + '_{' + str(args.pol) + '}' +  '.' + args.figfmt).format('T', 'Q', 'U', 'V')
+    else:
+        out_file = args.outfile or args.inmap.split('/')[-1].replace('.hdf5', '.' + args.figfmt)
 
     plt.figure(figsize=(args.figlength, args.figwidth))
-    ext = (lon_max + 360, lon_min + 360, lat_min, lat_max)
+    # ext = (lon_max + 360, lon_min + 360, lat_min, lat_max)
+    ext = (lon_max, lon_min, lat_min, lat_max)
     plt.imshow(cart_map, extent=ext, origin='lower', vmin=args.vmin, vmax=args.vmax)
     plt.xlabel('RA / deg')
     plt.ylabel('DEC / deg')
@@ -51,6 +56,8 @@ parser = argparse.ArgumentParser(description='Visualize a cartesian sky map.')
 parser.add_argument('inmap', type=str, help='Input cartesian map.')
 parser.add_argument('-o', '--outfile', type=str, nargs='?', help='Name of the image file to save into. If not present, the output image file name will be auto created from the input args.')
 parser.add_argument('--figfmt', default='png', help='Output image format.')
+parser.add_argument('-i', '--ifreq', type=int, default=0, help='Frequency channel to visualize (start from 0). Negative integer N means the last Nth channel.')
+parser.add_argument('-p', '--pol', type=int, default=0, choices=range(4), help='Polarization component to visualize, 0 for I/T, 1 for Q, 2 for U, 3 for V.')
 parser.add_argument('--vmin', type=float, help='The min value of the visualize range in the output image.')
 parser.add_argument('--vmax', type=float, help='The max value of the visualize range in the output image.')
 parser.add_argument('-l', '--figlength', type=float, default=8, help='Output figure length.')

00b5f38f16ebcabae673433e70857688db44796e
zuoshifan <zuoshifan@163.com>
2016-10-13 11:55:32 +0800

    Add orthview option in view_map

--- view_map.py
+++ view_map.py
@@ -55,14 +55,23 @@ def visualize_map(args):
         out_file = ((args.mapfiles[0].split('/')[-1]).split('.')[0] + '_' + str(ifreq) + '_{' + str(args.pol) + '}' +  '.' + args.figfmt).format('T', 'Q', 'U', 'V')
 
     # Plot and save image
-    fig = plt.figure(1, figsize=(args.figlength,args.figwidth))
+    if args.view == 'o':
+        fig = plt.figure(1, figsize=(8, 6))
+    else:
+        fig = plt.figure(1, figsize=(args.figlength,args.figwidth))
     map_data = hpmap[ifreq][args.pol]
     if args.sqrt:
         map_data = map_data / np.sqrt(np.abs(map_data))
+        map_data = map_data / np.sqrt(np.abs(map_data))
+        # map_data = map_data / np.sqrt(np.abs(map_data))
+        # import rotate
+        # map_data = rotate.rotate_map(map_data, rot=(0, -90, 0)) # rotate to make NCP at center
     if args.view == 'm':
         hpvisual.mollview(map_data, fig=1, title='', min=args.min, max=args.max)
     elif args.view == 'c':
         healpy.cartview(map_data, fig=1, title='', min=args.min, max=args.max)
+    elif args.view == 'o':
+        hpvisual.orthview(map_data, rot=(0, 90, 0), fig=1, title='', min=args.min, max=args.max, half_sky=True) # rot to make NCP at the center
     # fig = plt.figure()
     # ax = fig.add_axes()
     # cbar.solids.set_rasterized(True)
@@ -79,7 +88,7 @@ parser = argparse.ArgumentParser(description='Visualize a sky map in hdf5 files.
 parser.add_argument('mapfiles', type=str, nargs='+', help='Input hdf5 sky map files to visualize. If more than one, they will be first combined, i.e. added together.')
 parser.add_argument('-o', '--outfile', type=str, nargs='?', help='Name of the image file to save into. If not present, the output image file name will be auto created from the first input map file name and input args.')
 parser.add_argument('-f', '--figfmt', default='png', help='Output image format.')
-parser.add_argument('-v', '--view', type=str, choices=['m', 'c'], default='m', help='Which view, `m` for mollview, `c` for cartview.')
+parser.add_argument('-v', '--view', type=str, choices=['m', 'c', 'o'], default='m', help='Which view, `m` for mollview, `c` for cartview, `o` for orthview.')
 parser.add_argument('-s', '--sqrt', action='store_true', help='Plot by sqrt of the map.')
 parser.add_argument('-i', '--ifreq', type=int, default=0, help='Frequency channel to visualize (start from 0). Negative integer N means the last Nth channel.')
 parser.add_argument('-p', '--pol', type=int, default=0, choices=range(4), help='Polarization component to visualize, 0 for I/T, 1 for Q, 2 for U, 3 for V.')
